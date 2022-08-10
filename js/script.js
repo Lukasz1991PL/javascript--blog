30b7eb32f7e8a59f6c5030d7afbe0b528bb4e4a5
Lukasz Bartkowski <lukasz.bartkowski91@gmail.com>
2022-08-10 17:42:49 +0200

    add remove method for active posts

--- js/script.js
+++ js/script.js
@@ -1,25 +1,30 @@
-'use strict';
+"use strict";
+function titleClickHandler(event) {
+  console.log("Link was clicked!", event);
+}
+const links = document.querySelectorAll(".titles a");
 
-
-function titleClickHandler(event){
-  console.log('Link was clicked!', event);
-
-
-  /* remove class 'active' from all article links  */
-
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
-
-  /* get 'href' attribute from the clicked link */
-
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
+for (let link of links) {
+  link.addEventListener("click", titleClickHandler);
 }
 
-const links = document.querySelectorAll('.titles a');
+/* remove class 'active' from all article links  */
+const activeLinks = document.querySelectorAll(".titles a.active");
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+for (let activeLink of activeLinks) {
+  activeLink.classList.remove("active");
 }
+/* add class 'active' to the clicked link */
+
+/* remove class 'active' from all articles */
+const activeArticles = document.querySelectorAll(".posts article.active");
+
+for (let activeArticle of activeArticles) {
+  activeArticle.classList.remove("active");
+}
+
+/* get 'href' attribute from the clicked link */
+
+/* find the correct article using the selector (value of 'href' attribute) */
+
+/* add class 'active' to the correct article */
